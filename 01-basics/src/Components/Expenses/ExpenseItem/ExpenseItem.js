c6a0a43a6a7311057d4281f2ae6d68e0d8e62627
Sumit Khandelwal <sumitkhandelwal@Sumits-MacBook-Air.local>
2022-10-03 16:15:36 +0530

    expense item changed

--- 01-basics/src/Components/Expenses/ExpenseItem/ExpenseItem.js
+++ 01-basics/src/Components/Expenses/ExpenseItem/ExpenseItem.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function ExpenseItem(props) {
   return (
     <div>
-        <h6>{props.exp.title.toUpperCase()}</h6>
+        <h6>{props.exp.title.toUpperCase()} | {props.exp.id.toString()}</h6>
     </div>
   )
 }
